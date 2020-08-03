a0c71e0c6e6e20eb7d77e7d0b982534e7f7c3a3e
jianglinchen <jianglinchen123@163.com>
2020-08-03 11:32:49 +0800

    jianglinchen 1st commit

---
+++ src/main/java/cn/qphone/spring/test/TestDemo.java
@@ -0,0 +1,10 @@
+package cn.qphone.spring.test;
+
+/**
+ * @author Administrator
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!!!");
+    }
+}
